20f7ae9a6ca47052513ea441e8982ecd4ff2b586
Mayank Rajput <rituknj@gmail.com>
2022-12-05 18:14:07 +0530

    update-popup-card

--- src/Components/Navbars/Navbarnft.js
+++ src/Components/Navbars/Navbarnft.js
@@ -20,6 +20,9 @@ import Music from "./Navimg/Ellipse 5.png";
 import Photography from "./Navimg/Ellipse 6.png";
 import Videos from "./Navimg/Ellipse 7.png";
 import Imgback from "./Navimg/Rectangle 684.png";
+import Imgcarts from "./Navimg/cartimg.png";
+import Imgcarts1 from "./Navimg/cartimg2.png";
+import Checkicons from "./Navimg/che.png";
 
 // ICONS
 import { FaDiscord } from "react-icons/fa";
@@ -29,12 +32,16 @@ import { BsInstagram } from "react-icons/bs";
 import { BsTwitter } from "react-icons/bs";
 import { FaTiktok } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
 
 // IMPORT SWITCH TAB FOR NIGHT MODE
-import { styled } from '@mui/material/styles';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
+import { styled } from "@mui/material/styles";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
+import CheckIcon from "@mui/icons-material/Check";
 
 const IOSSwitch = styled((props) => (
   <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
@@ -42,46 +49,46 @@ const IOSSwitch = styled((props) => (
   width: 42,
   height: 26,
   padding: 0,
-  '& .MuiSwitch-switchBase': {
+  "& .MuiSwitch-switchBase": {
     padding: 0,
     margin: 2,
-    transitionDuration: '300ms',
-    '&.Mui-checked': {
-      transform: 'translateX(16px)',
-      color: '#fff',
-      '& + .MuiSwitch-track': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#FEC200',
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#FEC200",
         opacity: 1,
         border: 0,
       },
-      '&.Mui-disabled + .MuiSwitch-track': {
+      "&.Mui-disabled + .MuiSwitch-track": {
         opacity: 0.5,
       },
     },
-    '&.Mui-focusVisible .MuiSwitch-thumb': {
-      color: '#33cf4d',
-      border: '6px solid #fff',
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
     },
-    '&.Mui-disabled .MuiSwitch-thumb': {
+    "&.Mui-disabled .MuiSwitch-thumb": {
       color:
-        theme.palette.mode === 'light'
+        theme.palette.mode === "light"
           ? theme.palette.grey[100]
           : theme.palette.grey[600],
     },
-    '&.Mui-disabled + .MuiSwitch-track': {
-      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
     },
   },
-  '& .MuiSwitch-thumb': {
-    boxSizing: 'border-box',
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
     width: 22,
     height: 22,
   },
-  '& .MuiSwitch-track': {
+  "& .MuiSwitch-track": {
     borderRadius: 26 / 2,
-    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
     opacity: 1,
-    transition: theme.transitions.create(['background-color'], {
+    transition: theme.transitions.create(["background-color"], {
       duration: 500,
     }),
   },
@@ -92,6 +99,12 @@ function Navbar(props) {
   const [show1, setShow1] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [selectedValue, setSelectedValue] = React.useState("a");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
   return (
     <>
@@ -109,7 +122,7 @@ function Navbar(props) {
         <Grid item xl={9} lg={9} md={3} sm={3} xs={3} className="toggle-btn">
           <Navbaroffcanvas />
         </Grid>
-      {/*   <Grid
+        {/*   <Grid
           item
           xl={4}
           lg={3}
@@ -165,14 +178,31 @@ function Navbar(props) {
                 <li className="li-section d-flex">
                   <a className="dropdown-item" href="/">
                     Night Mode
-                  </a><div className={`form-check form-switch  text-${
-                        props.mode === "light" ? "dark" : "light"
-                      }`}> <span onClick={() => {
-                          setShow1(!show1);
-                        }}><FormControlLabel
-        control={<IOSSwitch sx={{ m: 1 }} defaultChecked  className="ios-switch" onClick={props.toggleMode} /> }
-        // label="iOS style"
-      /></span></div>
+                  </a>
+                  <div
+                    className={`form-check form-switch  text-${
+                      props.mode === "light" ? "dark" : "light"
+                    }`}
+                  >
+                    {" "}
+                    <span
+                      onClick={() => {
+                        setShow1(!show1);
+                      }}
+                    >
+                      <FormControlLabel
+                        control={
+                          <IOSSwitch
+                            sx={{ m: 1 }}
+                            defaultChecked
+                            className="ios-switch"
+                            onClick={props.toggleMode}
+                          />
+                        }
+                        // label="iOS style"
+                      />
+                    </span>
+                  </div>
                 </li>
               </ul>
             </div>
@@ -183,17 +213,21 @@ function Navbar(props) {
               <li type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src={carticon} alt="" />
               </li>
-              <ul className="dropdown-menu card-section image-bg" >
-                <li className="cart-menu">Haven’t found<br/> the right NFT? </li>
+              <ul className="dropdown-menu card-section image-bg">
+                <li className="cart-menu">
+                  Haven’t found
+                  <br /> the right NFT?{" "}
+                </li>
                 <li className="para-li">
                   Add NFTs to your bag to
                   <br /> check out and get hold of them.
                 </li>
-                <li><button className="card-section-button">Explore Recordian NFT</button></li>
                 <li>
-                  {/* <img src={Imgback} alt="" className="imgback"/> */}
+                  <button className="card-section-button">
+                    Explore Recordian NFT
+                  </button>
                 </li>
-                
+                <li>{/* <img src={Imgback} alt="" className="imgback"/> */}</li>
               </ul>
             </div>
           </ul>
@@ -270,13 +304,13 @@ function Navbar(props) {
                 Resources
               </li>
               <ul className="dropdown-menu">
-              <li className="li-section">
-                  <Link className="dropdown-item" to="/resourceshelpcenter" >
+                <li className="li-section">
+                  <Link className="dropdown-item" to="/resourceshelpcenter">
                     Help Center
                   </Link>
                 </li>
                 <li className="li-section">
-                  <Link className="dropdown-item" to="/collectioninsidepage" >
+                  <Link className="dropdown-item" to="/collectioninsidepage">
                     Collection-inside-page
                   </Link>
                 </li>
@@ -285,9 +319,9 @@ function Navbar(props) {
                    Newsletter
                   </Link>
                 </li> */}
-              
+
                 <li className="li-section">
-                  <a className="dropdown-item" href="/" >
+                  <a className="dropdown-item" href="/">
                     Docs
                   </a>
                 </li>
@@ -302,8 +336,8 @@ function Navbar(props) {
                   </a>
                 </li>
                 <li className="li-section">
-                <Link className="dropdown-item" to="/resourcesnewsletter" >
-                   Newsletter
+                  <Link className="dropdown-item" to="/resourcesnewsletter">
+                    Newsletter
                   </Link>
                 </li>
                 <li className="li-section">
@@ -341,6 +375,224 @@ function Navbar(props) {
   </ul> */}
             </div>
           </ul>
+          {/* <div className="cart-box-section">
+            <div className="row py-5 px-3">
+              <div className="col-6 mb-3">
+                <span className="beg-section ">
+                  Beg <strong className="num-redco">2</strong>
+                </span>
+              </div>
+              <div className="col-6 ">
+                <div className="icons-cross">
+                  <HighlightOffIcon />
+                </div>{" "}
+              </div>
+              <div className="row ">
+                <div className="col-4">
+                  <img src={Imgcarts} alt="" />
+                </div>
+                <div className="col-4" style={{ lineHeight: "12px" }}>
+                  <span className="loop-section">Loop(Common..)</span>
+                  <br />
+                  <span className="loop-section1">
+                    Red Angrybol
+                    <img src={Checkicons} alt="" />
+                  </span>
+
+                  <span className="loop-section1">Creator fee: 10%</span>
+                </div>
+                <div className="col-4">
+                  <span className="amount-cart-item">6.5667 ETH</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-4">
+                  <img src={Imgcarts1} alt="" />
+                </div>
+                <div className="col-4" style={{ lineHeight: "12px" }}>
+                  <span className="loop-section">Loop(Common..)</span>
+                  <br />
+                  <span className="loop-section1">
+                    Red Angrybol
+                    <img src={Checkicons} alt="" />
+                  </span>
+
+                  <span className="loop-section1">Creator fee: 10%</span>
+                </div>
+                <div className="col-4">
+                  <span className="amount-cart-item">6.5667 ETH</span>
+                </div>
+              </div>
+              <div className="row py-5 px-4">
+                <div className="section-eth-amount">
+                  <div className="col-6">
+                    <span>Total</span>
+                  </div>
+                  <div className="col-6 ethllo">
+                    <span>6.5667 ETH</span>
+                  </div>
+                </div>
+              </div>
+              <div className="row py-5 px-4">
+                <div className="section-eth-amount1">
+                  <span className="check-out11">Check Out</span>
+                </div>
+              </div>
+            </div>
+          </div> */}
+          {/* <div className="cart-box-section">
+            <div className="row py-5 px-3">
+              <div className="col-6 mb-3">
+                <span className="beg-section ">
+                  Beg <strong className="num-redco">2</strong>
+                </span>
+              </div>
+              <div className="col-6 ">
+                <div className="icons-cross">
+                  <HighlightOffIcon />
+                </div>{" "}
+              </div>
+              <div className="row ">
+                <div className="col-4">
+                  <img src={Imgcarts} alt="" />
+                </div>
+                <div className="col-4" style={{ lineHeight: "12px" }}>
+                  <span className="loop-section">Loop(Common..)</span>
+                  <br />
+                  <span className="loop-section1">
+                    Red Angrybol
+                    <img src={Checkicons} alt="" />
+                  </span>
+                  <span className="loop-section1">Creator fee: 10%</span>
+                </div>
+                <div className="col-4">
+                  <span className="amount-cart-item">6.5667 ETH</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-4">
+                  <img src={Imgcarts1} alt="" />
+                </div>
+                <div className="col-4" style={{ lineHeight: "12px" }}>
+                  <span className="loop-section">Loop(Common..)</span>
+                  <br />
+                  <span className="loop-section1">
+                    Red Angrybol
+                    <img src={Checkicons} alt="" />
+                  </span>
+                  <span className="loop-section1">Creator fee: 10%</span>
+                </div>
+                <div className="col-4">
+                  <span className="amount-cart-item">6.5667 ETH</span>
+                </div>
+              </div>
+              <div className="row py-5 px-4">
+                <div className="section-eth-amount">
+                  <div className="col-6">
+                    <span>Total</span>
+                  </div>
+                  <div className="col-6 ethllo">
+                    <span>6.5667 ETH</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="row">
+                  <span>Payment method</span>
+                  <div className="col-3">
+                    <div>
+                      <Radio
+                        checked={selectedValue === "a"}
+                        onChange={handleChange}
+                        value="a"
+                        name="radio-buttons"
+                        inputProps={{ "aria-label": "A" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-9">
+                    <span>CRYPTO</span>
+                    <br />
+                    <span>Ethereum</span>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-3">
+                    <div>
+                      <Radio
+                        checked={selectedValue === "a"}
+                        onChange={handleChange}
+                        value="a"
+                        name="radio-buttons"
+                        inputProps={{ "aria-label": "A" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-9">
+                    <span>Credit or debit card</span>
+                    <br />
+                    <span>*******9095</span>
+                  </div>
+                </div>
+              </div>
+              <div className="row py-5 px-4">
+                <div className="section-eth-amount1">
+                  <span className="check-out11">Complete Purchase</span>
+                </div>
+              </div>
+            </div>
+          </div> */}
+          {/* <div className="cart-box-section">
+            <div className="row py-5 px-3">
+              <div className="col-6 mb-3">
+                
+              </div>
+              <div className="col-6 ">
+                <div className="icons-cross">
+                  <HighlightOffIcon />
+                </div>{" "}
+              </div>
+              <div className="row ">
+                <div className="chek">
+                  <CheckIcon className="cor-dd" />
+                </div>
+                <div className="pur-div">
+                  <span className="purchse-com">Purchase complete</span>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-6">
+                  <span className="item-se">Items:</span>
+                </div>
+                <div className="col-6">
+                  <span className="item-se">(2) Loop, Cloudball</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <span className="item-se">SubTotal:</span>
+                </div>
+                <div className="col-6">
+                  <span className="item-se">6.5667 ETH</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <span className="item-se">Paid with:</span>
+                </div>
+                <div className="col-6">
+                  <span className="item-se">Credit or debit card</span>
+                </div>
+              </div>
+
+              <div className="row py-5 px-4">
+                <div className="hhun">
+                  <span className="check-out122">Back to Explore page</span>
+                </div>
+              </div>
+            </div>
+          </div> */}
         </Grid>
       </Grid>
     </>
