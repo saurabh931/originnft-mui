5c35444a6a0c4740f682743dd52b6fc1d401a5f9
rituknj <rsyadav.abss1@gmail.com>
2022-12-05 20:32:47 +0530

    home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image1 from "../Images/mainslider1.png";
 import Image2 from "../Images/mainslider2.png";
@@ -12,106 +12,116 @@ import Image8 from "../Images/mainslider4.png";
 import Image9 from "../Images/mainslider1.png";
 import { Link as RouterLink } from "react-router-dom";
 import { Link } from "@mui/material";
-import Hometabs from "./Hometabs"
+import Hometabs from "./Hometabs";
 import "swiper/css";
 import "swiper/css/pagination";
-
-
 
 // import required modules
 import { Autoplay, Navigation, Pagination } from "swiper";
 
-
 export default function Home() {
   return (
     <>
-    <div className="Home">
-    <div className='container-fluid background'>
-    <div>
-      <h1 className='header'>Explore, collect, and sell NFTs</h1>
-      <div className='main-button'> <Link className='button-primary mx-3' id='button-1' to="/explore" component={RouterLink}>Explore</Link>
-      <Link className='button-secondary' id='button-2' to='/exploreinsidepage'  component={RouterLink}>Sell</Link></div>
-    </div>
-    <div className="container-fluid py-5  swiper213 ">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={10}
-          pagination={{
-            clickable: false,
-          }}
-          navigation={{
-            clickable: false,
-          }}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-              // spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 3,
-              // spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 4,
-              // spaceBetween: 50,
-            },
-            1920:{
-              slidesPerView: 4,
-              // spaceBetween: 50,
-            }
-          }}
-          modules={[ Navigation, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img
-              src={Image1}
-              alt=""
-              className="slider1"
-             
-             
-            />
-          </SwiperSlide>
-          <SwiperSlide >
-            {" "}
-            <img src={Image2} alt="" className="slider1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={Image3} alt="" className="slider1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={Image4} alt="" className="slider1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={Image5} alt="" className="slider1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={Image6} alt="" className="slider1" />
-          </SwiperSlide>
-          {/* <SwiperSlide>
+      <div className="Home">
+        <div className="container-fluid background">
+          <div>
+            <h1 className="header">Explore, collect, and sell NFTs</h1>
+            <div className="main-button">
+              {" "}
+              <Link
+                className="button-primary mx-3"
+                id="button-1"
+                to="/explore"
+                component={RouterLink}
+              >
+                Explore
+              </Link>
+              <Link
+                className="button-secondary"
+                id="button-2"
+                to="/exploreinsidepage"
+                component={RouterLink}
+              >
+                Sell
+              </Link>
+            </div>
+          </div>
+          <div className="container-fluid py-5  swiper213 ">
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: false,
+              }}
+              navigation={{
+                clickable: false,
+              }}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  // spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  // spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  // spaceBetween: 50,
+                },
+                1920: {
+                  slidesPerView: 4,
+                  // spaceBetween: 50,
+                },
+              }}
+              modules={[Navigation, Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img src={Image1} alt="" className="slider1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <img src={Image2} alt="" className="slider1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <img src={Image3} alt="" className="slider1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <img src={Image4} alt="" className="slider1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <img src={Image5} alt="" className="slider1" />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <img src={Image6} alt="" className="slider1" />
+              </SwiperSlide>
+              {/* <SwiperSlide>
             {" "}
             <img src={Image7} alt="" className="slider1" />
           </SwiperSlide> */}
-         {/*  <SwiperSlide>
+              {/*  <SwiperSlide>
             {" "}
             <img src={Image8} alt="" className="slider1" />
           </SwiperSlide> */}
-         {/*  <SwiperSlide>
+              {/*  <SwiperSlide>
             <img src={Image9} alt="" className="slider1" />
           </SwiperSlide> */}
-        </Swiper>
+            </Swiper>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    <div><Hometabs/></div>
+      <div>
+        <Hometabs />
+      </div>
     </>
-  )
+  );
 }
